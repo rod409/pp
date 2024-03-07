15d68a3e1e3827d4b53e5e391255e5c9d6509ffb
rod409 <radoyeh@gmail.com>
2024-03-07 13:44:07 -0800

    Correctly get all camera_sync labels -requires waymo-open-dataset-tf-2-11-0==1.6.1

--- data_prep/waymo_converter.py
+++ data_prep/waymo_converter.py
@@ -228,7 +228,7 @@ class Waymo2KITTI(object):
             file_idx (int): Current file index.
             frame_idx (int): Current frame index.
         """
-        range_images, camera_projections, range_image_top_pose = \
+        range_images, camera_projections, segmentation_labels, range_image_top_pose = \
             parse_range_image_and_camera_projection(frame)
 
         if range_image_top_pose is None:
@@ -328,7 +328,7 @@ class Waymo2KITTI(object):
             # e.g.: the projected 2D labels can be in camera 2
             # while the most_visible_camera can have id 4
             if cam_sync:
-                if hasattr(obj, 'most_visible_camera_name'):
+                if obj.camera_synced_box.ByteSize():
                     name = str(
                         self.cam_list.index(
                             f'_{obj.most_visible_camera_name}'))
