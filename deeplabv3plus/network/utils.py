4f98741c1aa08a5b374237780f17af6544b1910d
rod409 <radoyeh@gmail.com>
2025-05-02 10:59:52 -0700

    modify deeplab to directly output sgementation scores for inclusion of max in onnx graph

--- deeplabv3plus/network/utils.py
+++ deeplabv3plus/network/utils.py
@@ -15,7 +15,7 @@ class _SimpleSegmentationModel(nn.Module):
         features = self.backbone(x)
         x = self.classifier(features)
         x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
-        return x
+        return x.max(dim=1)[1]
 
 
 class IntermediateLayerGetter(nn.ModuleDict):
